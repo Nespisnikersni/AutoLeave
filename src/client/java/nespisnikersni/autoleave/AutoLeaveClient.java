215ab97dcd61cdb1dfccc0b7abef1f3feb4aeb9d
Nespisnikersni <143795816+Nespisnikersni@users.noreply.github.com>
2024-06-09 00:05:45 +0600

    Add files via upload

---
+++ src/client/java/nespisnikersni/autoleave/AutoLeaveClient.java
@@ -0,0 +1,36 @@
+package nespisnikersni.autoleave;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class AutoLeaveClient implements ClientModInitializer {
+	private ConfigA config = new ConfigA();
+	public static KeyBinding keyBinding;
+	@Override
+	public void onInitializeClient() {
+		config.createConfig("AutoLeave");
+		if(config.isEmpty("AutoLeave")){
+			config.addString("AutoLeave","enabled",String.valueOf(true));
+			config.addString("AutoLeave","health","5");
+		}
+		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("menu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_P, "AutoLeave"));
+		ClientTickEvents.END_CLIENT_TICK.register(new ClientTickEvents.EndTick() {
+			@Override
+			public void onEndTick(MinecraftClient client) {
+                if(client.player != null&&client.world!=null&&Boolean.valueOf(config.getString("AutoLeave","enabled"))&&client.player.getHealth()<=Integer.valueOf(config.getString("AutoLeave","health"))){
+					client.world.disconnect();
+				}
+				while(keyBinding.wasPressed()){
+					Menu menu = new Menu(Text.literal("aesf"));
+					client.setScreen(menu);
+				}
+			}
+		});
+	}
+}
